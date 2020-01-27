41d51d33e6617802a7bc7071a848ce3f2c2fac98
Serhii <kyivancard6@yahoo.com>
2020-01-27 11:56:27 +0200

    First Commit

---
+++ src/main/java/com/mypackage/Addition.java
@@ -0,0 +1,14 @@
+package com.mypackage;
+
+public class Addition {
+    public static void main(String... pars) {
+        int a = 5;
+        int b = 6;
+        add(a, b);
+    }
+
+    // addition
+    private static void add(int x, int y) {
+        System.out.println(x + y);
+    }
+}
